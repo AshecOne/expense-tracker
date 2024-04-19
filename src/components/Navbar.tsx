814ac49649d329dbe18ee853c271393f6db3690f
AshecOne <haseyong123@gmail.com>
2024-04-19 11:05:31 +0700

    fixing layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,41 +13,22 @@ const Navbar: React.FunctionComponent<INavbarProps> = (props) => {
       {!isDashboard && (
         <div className="fixed top-0 right-0 m-4">
           <Link href="/dashboard" aria-label="Dashboard">
-            <FiHome
-              size={24}
-              className="text-black hover:text-blue-600 transition-colors duration-200"
-            />
+            <FiHome size={24} className="text-black hover:text-blue-600 transition-colors duration-200" />
           </Link>
         </div>
       )}
       <nav className="bg-white shadow-lg fixed inset-x-0 bottom-0 z-10">
         <div className="px-5 py-3 flex justify-around items-center">
-          <Link
-            href="/sortir"
-            aria-label="Sortir"
-            className="focus:outline-none hover:text-blue-600 transition-colors duration-200"
-          >
-            <FiFile size={24} className="text-black" />
+          <Link href="/sortir" aria-label="Sortir" className="focus:outline-none hover:text-blue-600 transition-colors duration-200">
+            <FiFile size={24} className="text-black hover:text-blue-600 transition-colors duration-200" />
           </Link>
-          <Link
-            href="/input"
-            aria-label="Input"
-            className="focus:outline-none hover:text-red-700 transition-colors duration-200"
-          >
+          <Link href="/input" aria-label="Input" className="focus:outline-none hover:text-red-700 transition-colors duration-200">
             <div className="w-12 h-12 rounded-full bg-red-600 flex items-center justify-center">
-              <FiPlus
-                size={24}
-                color="white"
-                className="hover:text-red-700 transition-colors duration-200"
-              />
+              <FiPlus size={24} color="white" className="hover:text-red-700 transition-colors duration-200" />
             </div>
           </Link>
-          <Link
-            href="/profile"
-            aria-label="Profil"
-            className="focus:outline-none hover:text-blue-600 transition-colors duration-200"
-          >
-            <FiUser size={24} className="text-black" />
+          <Link href="/profile" aria-label="Profil" className="focus:outline-none hover:text-blue-600 transition-colors duration-200">
+            <FiUser size={24} className="text-black hover:text-blue-600 transition-colors duration-200" />
           </Link>
         </div>
       </nav>
