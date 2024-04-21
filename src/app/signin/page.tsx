315e93473b7dc8a991dbcd0d863d7e727c702b9d
AshecOne <haseyong123@gmail.com>
2024-04-21 17:46:49 +0700

    fix toastify

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -39,14 +39,16 @@ const SignIn: React.FunctionComponent<ISignInProps> = (props) => {
         localStorage.setItem("user", JSON.stringify(response.data.user));
       }
       console.log("Before showing success toast");
-      toast.success("Sign in successful!", {
-        autoClose: 2000,
-        onClose: () => {
-          console.log("Navigating to dashboard");
-          router.push("/dashboard");
-        },
-      });
-      
+      setTimeout(() => {
+        toast.success("Sign in successful!", {
+          autoClose: 2000,
+          onClose: () => {
+            console.log("Success toast closed");
+            console.log("Navigating to dashboard");
+            router.push("/dashboard");
+          },
+        });
+      }, 0);
       console.log("After showing success toast");
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
