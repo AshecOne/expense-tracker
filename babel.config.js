385c03ec9ba2846b777311c07707226d2b8f7414
AshecOne <haseyong123@gmail.com>
2024-04-25 04:12:29 +0700

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,16 @@
-module.exports = function (api) {
-  api.cache(true);
+module.exports = function(api) {
+    const isProduction = api.env("production");
+  
+    const plugins = [];
+  
+    if (isProduction) {
 
-  const plugins = [];
-
-  if (api.env("production")) {
-    plugins.push("transform-remove-console");
-  }
-
-  return {
-    presets: ["next/babel"],
-    plugins: plugins,
+      plugins.push("transform-remove-console");
+    }
+  
+    return {
+      presets: ["next/babel"], 
+      plugins: plugins,
+    };
   };
-};
+  
