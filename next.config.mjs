d1de9efe9a7ecf81ae465f23f95e697196cd841e
AshecOne <haseyong123@gmail.com>
2024-04-24 13:36:28 +0700

    beda format

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
-/** @type {import('next').NextConfig} */
+// Ubah baris ini
 require('dotenv').config({ path: '.env.local' });
+
+// Menjadi seperti ini
+import { config } from 'dotenv';
+config({ path: '.env.local' });
+
 const nextConfig = {};
 
 export default nextConfig;
